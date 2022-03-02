15758915752cfc3ec84b868e9616c91d480167a8
Leonid Bogdanov <leonid_bogdanov@mail.ru>
2022-03-03 10:24:58 +1100

    Implement track conversion task

--- utils/tasks.py
+++ utils/tasks.py
@@ -3,14 +3,17 @@ import os
 from cue_parser import parse_cue_sheet
 from hashlib import sha1
 from invoke import Exit, task
-from itertools import zip_longest
+from itertools import chain, zip_longest
 from pathlib import Path
+from random import shuffle
 from shlex import quote
 
 
 ALLOWED_TAGS = {
-    "album", "artist", "date", "genre", "title", "tracknumber", "tracktotal"
+    "album", "artist", "date", "discnumber", "disctotal",
+    "genre", "title", "tracknumber", "tracktotal"
 }
+AUDIO_FILES = {".flac", ".wav"}
 
 
 def _error(msg, **kwargs): print(f"\033[33m{msg}\033[0m", **kwargs)
@@ -41,7 +44,7 @@ def clean_tags(ctx, in_dir):
         print(f"Scanning '{cur_dir}'...", end="")
 
         flac_files = [
-            f for f in map(Path, files) if f.suffix.casefold() == ".flac"
+            f for f in map(Path, files) if f.suffix.lower() == ".flac"
         ]
 
         if not flac_files:
@@ -58,11 +61,11 @@ def clean_tags(ctx, in_dir):
                 f"metaflac --export-tags-to - {cur_file}", hide=True
             )
             exported_tags = {
-                kv[0].lower(): kv[1]
+                kv[0].lower(): kv[1].strip()
                 for kv in (
                     t.split("=", 1) for t in exported_tags.stdout.split("\n")
                 )
-                if len(kv) == 2
+                if len(kv) == 2 and kv[1].strip()
             }
 
             ctx.run(f"metaflac --dont-use-padding --remove-all {cur_file}")
@@ -113,7 +116,7 @@ def split_files(ctx, in_dir, out_dir, checksum=False):
         print(f"Scanning '{cur_dir}'...", end="")
 
         cue_files = [
-            f for f in map(Path, files) if f.suffix.casefold() == ".cue"
+            f for f in map(Path, files) if f.suffix.lower() == ".cue"
         ]
 
         if not cue_files:
@@ -192,3 +195,67 @@ def split_files(ctx, in_dir, out_dir, checksum=False):
             for sha1hash, file in hashes:
                 f.write(f"{sha1hash}  {os.curdir}{os.sep}{file}\n")
         print("Checksum file created.")
+
+@task(
+    help={
+        "in-dir": "Source directories",
+        "out-dir": "Output directory",
+        "limit": "Limit of files to convert"
+    },
+    iterable=["in_dir"]
+)
+def random_tracks(ctx, in_dir, out_dir, limit=100):
+    """
+    Given a list of directories, traverses directories and converts
+    found audio files into MP4 format.
+
+    Requires FFmpeg to be installed.
+    """
+
+    def to_valid_dir(d):
+        d = Path(d)
+        if not d.is_dir():
+            raise Exit(message=f"'{d}' is not valid directory!")
+        return d
+
+    def find_audio_files(in_dir):
+        for cur_dir, _, files in os.walk(in_dir):
+            print(f"Scanning '{cur_dir}'...")
+
+            for f in files:
+                f = Path(cur_dir, f)
+                if f.suffix.lower() in AUDIO_FILES:
+                    yield f
+
+    in_dir, out_dir = map(to_valid_dir, in_dir), Path(out_dir)
+
+    if out_dir.is_dir() and any(out_dir.iterdir()):
+        raise Exit(message=f"'{out_dir}' is not empty directory!")
+    out_dir.mkdir(parents=True)
+
+    audio_files = list(
+        chain.from_iterable(
+            map(find_audio_files, in_dir)
+        )
+    )
+    shuffle(audio_files)
+    audio_files = audio_files[:limit]
+
+    print(
+        f"Converting {len(audio_files)} audio files...", end=" "
+    )
+
+    convert_args = "-c:a aac_at -q:a 5"
+
+    for idx, audio_file in enumerate(audio_files, start=1):
+        out_file = Path(out_dir, f"Track {idx}.m4a")
+        convert_result = ctx.run(
+            f"ffmpeg -hide_banner -i {_shq(audio_file)} {convert_args} {_shq(out_file)}",
+            hide=True, warn=True
+        )
+        if convert_result.ok:
+            print("*", end="", flush=True)
+        else:
+            _error("*", end="", flush=True)
+
+    print(" OK.")
